1daaf528825ed5e329a3ebbcdf467a3d5fe22628
Sharon <sharonli@iscom.com.tw>
2022-08-27 13:25:05 +0800

    資料庫初次建檔

---
+++ Database/Script.InserDefaultData.sql
@@ -0,0 +1,20 @@
+﻿/*
+部署後指令碼樣板
+--------------------------------------------------------------------------------------
+ 此檔案包含要附加到組建指令碼的 SQL 陳述式
+ 使用 SQLCMD 語法可將檔案包含在部署後指令碼中
+ 範例:      :r .\myfile.sql
+ 使用 SQLCMD 語法可參考部署後指令碼中的變數
+ 範例:      :setvar TableName MyTable
+               SELECT * FROM [$(TableName)]
+--------------------------------------------------------------------------------------
+*/
+
+IF NOT EXISTS(SELECT 1 FROM [dbo].[User] WHERE [Account] = 'admin')
+BEGIN
+    INSERT INTO [dbo].[User] ([Id],[Account],[Pwd],[Name],[Email],[Role],[Status],[CreateTime],[UpdateTime]) 
+    VALUES (
+        (SELECT ISNULL(MAX([Id]), 0) FROM [dbo].[User])+1,
+        'admin','toolsdefault123', N'系統管理員', 'admin@gmail.com', 2, 1, GETDATE(), GETDATE())
+END
+GO
---
+++ Database/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL , 
+    [Account] NVARCHAR(256) NOT NULL, 
+    [Pwd] NVARCHAR(256) NOT NULL, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    [Email] NVARCHAR(256) NOT NULL, 
+    [Role] INT NOT NULL, 
+    [Status] INT NOT NULL, 
+    [CreateTime] DATETIME NOT NULL, 
+    [UpdateTime] DATETIME NOT NULL
+)
+
+GO
